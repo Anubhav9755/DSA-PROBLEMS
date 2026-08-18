d72278cfb0a3a38148e0b386885bdbe15c0076c1
Anubhav Tiwari <2k24.cs.1d.2413386@gmail.com>
2026-08-18 14:19:26 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (78.15%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -1,32 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> ans;
-        if (root == nullptr) return ans;
-        
-        queue<TreeNode*> q;
-        q.push(root);
-        bool L2R = true; 
-        
-        while (!q.empty()) {
-            int s = q.size();
-            vector<int> level(s); 
-            
-            for (int i = 0; i < s; i++) {
-                TreeNode* node = q.front();
-                q.pop();
-    
-                int index = L2R ? i : (s - 1 - i);
-                level[index] = node->val;
-                
-                if (node->left) 
-                    q.push(node->left);
-                if (node->right) 
-                    q.push(node->right);
+        vector<vector<int>>ans;
+
+        if(root==nullptr)
+            return ans;
+        deque<TreeNode*>dq;
+        dq.push_back(root);
+
+        bool l2r = true;
+
+        while(!dq.empty()){
+            int n = dq.size();
+            vector<int>level;
+
+            for(int i=0;i<n;i++){
+                if(l2r){
+                    TreeNode* node = dq.front();
+                    dq.pop_front();
+
+                    level.push_back(node->val);
+
+                    if(node->left)
+                        dq.push_back(node->left);
+
+                    if(node->right)
+                        dq.push_back(node->right);
+                }
+                else{
+                    TreeNode* node = dq.back();
+                    dq.pop_back();
+
+                    level.push_back(node->val);
+
+                    if(node->right)
+                        dq.push_front(node->right);
+                    
+                    if(node->left)
+                        dq.push_front(node->left);
+                }
             }
-            
+
             ans.push_back(level);
-            L2R = !L2R; 
+            l2r=!l2r;
         }
         return ans;
     }
