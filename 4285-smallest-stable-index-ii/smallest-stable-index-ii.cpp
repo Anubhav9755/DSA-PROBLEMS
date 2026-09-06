07d0dbc48b95f7fc5e76aab3dc9276f8eb2a5f77
Anubhav Tiwari <2k24.cs.1d.2413386@gmail.com>
2026-09-06 16:48:11 +0530

    Time: 20 ms (17.22%) | Memory: 210.8 MB (29.92%) - LeetSync

--- 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
@@ -2,21 +2,19 @@ class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n = nums.size();
-
-        int ansIdx = 0;      
-        int globalMax = INT_MIN;    
-        int ansMax = INT_MIN;   
-
-        for(int i = 0; i < n; i++){
-            globalMax = max(globalMax, nums[i]);
-            if(i == ansIdx)
-                ansMax = max(ansMax, nums[i]);
-            if(nums[i] < ansMax - k){
-                ansIdx = i + 1;
-                ansMax = globalMax;
-            }
+        vector<int> mx(n, -1e9), mn(n, 1e9);
+        int mxi = nums[0], mni = nums[n-1];
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > mxi) mxi = nums[i] ;
+            mx[i] = mxi;
         }
-
-        return ansIdx < n ? ansIdx : -1;
+        for (int i = n-1; i >= 0; i--) {
+            if (nums[i] < mni) mni = nums[i] ;
+            mn[i] = mni;
+        }
+        for (int i = 0; i < n; i++) {
+            if ((mx[i] - mn[i]) <= k) return i ;
+        }
+        return -1;
     }
 };
